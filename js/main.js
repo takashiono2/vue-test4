0fa24902b07929de3acd43bed5599d610938e5a3
takashiono2 <takashiono710@gmail.com>
2021-06-22 18:06:48 +0900

    firstcommit

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    bpi: null,
-    hasError: false,
-    loading: true
-  },
-  mounted: function(){
-    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-    .then(function(response){
-      // console.log(response.data.bpi)
-      // console.log(response.data.bpi.USD.rate_float)
-      this.bpi = response.data.bpi
-    }.bind(this))
-    .catch(function(error){
-      console.log(error);
-      this.hasError = true;
-    }.bind(this))
-    .finally(function(error){
-      this.loading= false;
-    }.bind(this))
-  },
-  filters: {
-    currencyDecimal(value){
-      return value.toFixed(2);
-    }
-  }
+  el: '#app'
 });
