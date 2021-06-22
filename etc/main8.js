fef67aef5e2aedf91a5726f198be1abfc6b6a598
takashiono2 <takashiono710@gmail.com>
2021-06-23 02:31:26 +0900

    フィルタの引数

---
+++ etc/main8.js
@@ -0,0 +1,15 @@
+//グローバルフィルタ
+Vue.filter('toUSD',function(jpy){
+      return jpy/100;
+    });
+
+Vue.filter('numberFormat',function(value){
+      return value.toLocaleString();
+    });
+
+var app = new Vue({
+  el: '#app',
+  data:{
+    jpyPrice: 298000000
+  }
+});
--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
-//グローバルフィルタ
-Vue.filter('toUSD',function(jpy){
-      return jpy/100;
-    });
-
-Vue.filter('numberFormat',function(value){
-      return value.toLocaleString();
+Vue.filter('readMore',function(text,length,suffix){
+      return text.substring(0,length) + suffix;
     });
 
 var app = new Vue({
   el: '#app',
   data:{
-    jpyPrice: 298000000
+    text:'aaa bbb ccc ddd eee fff ggg hhh iii jjj kkk lll'
   }
 });
