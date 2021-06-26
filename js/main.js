17af8cba9e313d7742b5116319e4061fab2d9076
takashiono2 <takashiono710@gmail.com>
2021-06-27 00:43:06 +0900

    算出プロパティと監視プロパティの比較

--- js/main.js
+++ js/main.js
@@ -1,25 +1,48 @@
 var app = new Vue({
   el: '#app',
-  data: { 
-    km: 0,
-    m: 0,
-    mm: 0
+  data: {
+    firstName: '',
+    lastName: ''
+    // fullName: ''
   },
-  watch: {
-    km: function(value){
-      this.km = value;
-      this.m = value*1000;
-      this.mm = value*1000000;
-    },
-    m: function(value){
-      this.km = value/1000;
-      this.m = value;
-      this.mm = value*1000;
-    },
-    mm: function(value){
-      this.km = value/1000000;
-      this.m = value/1000;
-      this.mm = value;
-    }  
+  // watch: {
+  //   firstName: function(value){
+  //     this.fullName = value + '' + this.lastName;
+  //   },
+  //   lastName: function(value){
+  //     this.fullName = this.firstName + '' + value;
+  //   }
+  // },
+  computed: {
+    fullName: function(){
+      return this.firstName + '' + this.lastName
+    }
   }
 });
+
+// No.83 単位変換アプリ
+// var app = new Vue({
+//   el: '#app',
+//   data: { 
+//     km: 0,
+//     m: 0,
+//     mm: 0
+//   },
+//   watch: {
+//     km: function(value){
+//       this.km = value;
+//       this.m = value*1000;
+//       this.mm = value*1000000;
+//     },
+//     m: function(value){
+//       this.km = value/1000;
+//       this.m = value;
+//       this.mm = value*1000;
+//     },
+//     mm: function(value){
+//       this.km = value/1000000;
+//       this.m = value/1000;
+//       this.mm = value;
+//     }  
+//   }
+// });
