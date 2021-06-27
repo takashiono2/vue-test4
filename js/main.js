8bf4221f78698c55616201752863ba13369fa2ff
takashiono2 <takashiono710@gmail.com>
2021-06-27 22:54:44 +0900

    テンプレート側の実装

--- js/main.js
+++ js/main.js
@@ -1,25 +1,65 @@
 var app = new Vue({
   el: '#app',
-  data: {
-    colors: [
-      {name:'red'},
-      {name:'green'},
-      {name:'blue'}
-    ]
+  data:{
+    items: null,
+    keyword: '',
+    message: ''
   },
   watch: {
-    colors: {
-      handler: function(newValue,oldValue){
-        console.log('Update!')
-        console.log('newValue: %s,oldValue: %s',
-          JSON.stringify(newValue,null, '\t'),
-          JSON.stringify(oldValue,null, '\t'))
-      },
-    deep: true,
-    immediate: true
-    }
+
+  },
+  created: function(){
+    this.keyword = 'JavaScript'
+    this.getAnswer()
+  },
+　methods:{
+    getAnswer: function(){
+      if(this.keyword===''){
+        this.items = null
+        return
+      }//キーワードが空なら終了
+
+    this.message = 'Loading...'
+    var vm = this//axiosでVueインスタンスのプロパティにアクセスするため、thisをローカル変数に入れておく
+    var params = {page:1, per_page: 20, query:this.keyword}//パラメーターをキーバーリューの形式で用意
+    axios.get('https://qiita.com/api/v2/items',{params})
+    .then(function(response){
+      console.log(response)
+      vm.items = response.data
+    })
+    .catch(function(error){
+      vm.message = 'Error!' + error
+    })
+    .finally(function(){
+      vm.message = ''
+    })
   }
+}
 });
+
+// No.88 オプションimmediate
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     colors: [
+//       {name:'red'},
+//       {name:'green'},
+//       {name:'blue'}
+//     ]
+//   },
+//   watch: {
+//     colors: {
+//       handler: function(newValue,oldValue){
+//         console.log('Update!')
+//         console.log('newValue: %s,oldValue: %s',
+//           JSON.stringify(newValue,null, '\t'),
+//           JSON.stringify(oldValue,null, '\t'))
+//       },
+//     deep: true,
+//     immediate: true
+//     }
+//   }
+// });
 
 // No.84 算出プロパティと監視プロパティの比較
 // var app = new Vue({
