e6e398ddb6165d56c2a426a8f181bb83ced0dffa
takashiono2 <takashiono710@gmail.com>
2021-07-03 18:03:18 +0900

    157リダイレクト

--- js/main.js
+++ js/main.js
@@ -1,48 +1,72 @@
-const User = { 
+const B = { 
   template: `
-    <div class="user">
-      <h2>User {{$route.params.userId}}</h2>
-      <router-view></router-view>
+    <div>
+      <h2>This is B.</h2>
     </div>
   `
   }
 
-  const Profile = { 
-    template: `
-      <div class="profile">
-        <h2>Profile {{ $route.params.userId }}</h2>
-      </div>
-    `
-    }
-
-    const Posts = { 
-      template: `
-        <div class="posts">
-          <h2>Posts {{ $route.params.userId }}</h2>
-        </div>
-      `
-      }
-
 const router = new VueRouter({
   routes:[
     { 
-    path: '/Users/:userId',
-    component: User,
-      children: [
-        {  
-        path: 'profile',
-        component: Profile
-        },
-        {  
-        path: 'posts',
-        component: Posts
-        },
-      ]
+      path: '/a',
+      redirect: '/b'
+    },
+    { 
+      path: '/b',
+      component: B
     }
   ]
 })
 
 const app = new Vue({router: router}).$mount('#app')
+
+// No156ネストされたルート
+// const User = { 
+//   template: `
+//     <div class="user">
+//       <h2>User {{$route.params.userId}}</h2>
+//       <router-view></router-view>
+//     </div>
+//   `
+//   }
+
+//   const Profile = { 
+//     template: `
+//       <div class="profile">
+//         <h2>Profile {{ $route.params.userId }}</h2>
+//       </div>
+//     `
+//     }
+
+//     const Posts = { 
+//       template: `
+//         <div class="posts">
+//           <h2>Posts {{ $route.params.userId }}</h2>
+//         </div>
+//       `
+//       }
+
+// const router = new VueRouter({
+//   routes:[
+//     { 
+//     path: '/Users/:userId',
+//     component: User,
+//       children: [
+//         {  
+//         path: 'profile',
+//         component: Profile
+//         },
+//         {  
+//         path: 'posts',
+//         component: Posts
+//         },
+//       ]
+//     }
+//   ]
+// })
+
+// const app = new Vue({router: router}).$mount('#app')
 
 // No.155名前付きのルート
 // const Top = { template: '<div>Top</div>' }
